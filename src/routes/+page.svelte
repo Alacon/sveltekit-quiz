6ce116bc8999e5b6345818002cc095a51bb19b15
Pelle Edlund <pelle.edlund@minddig.com>
2022-09-13 08:15:10 +0200

    Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-full h-[90vh] flex flex-col justify-center items-center">
+	<h1 class="text-3xl">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { resolve } from 'path';
 
 const config: UserConfig = {
+	resolve: {
+		alias: {
+		  $src: resolve("./src"),
+		  $models: resolve("./src/models"),
+		  $components: resolve("./src/components"),
+		},
+	  },
 	plugins: [sveltekit()]
 };
 
