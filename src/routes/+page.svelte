d17f05e62ca6ac5a4c81baf367a6c9ea23a540f4
Pelle Edlund <pelle.edlund@minddig.com>
2022-09-17 12:38:24 +0200

    loading screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,45 +30,60 @@
 	};
 
 	$: answer = '';
+	$: happy = true;
 
 	onMount(() => {
 		fetch('/question').then(async (x) => {
 			const questions = await x.json();
 			smallQuestions.set(questions);
 		});
+		setTimeout(() => {
+			happy = false;
+		}, 3500);
 	});
 </script>
 
-<div
-	class="w-full h-full flex flex-col justify-center items-center gap-y-10 transition-colors duration-1000 ease-in-out"
-	class:bg-red-500={state == State.Wrong}
->
-	{#if state == State.Corerct}
+{#if happy}
+	<div
+		class="w-full h-full flex flex-col justify-center items-center gap-y-10 transition-colors duration-1000 ease-in-out"
+		class:bg-red-500={state == State.Wrong}
+	>
 		<ConfettiExplosion />
-	{/if}
-	<h1 class="text-3xl px-5 text-center">
-		{$currentQuestion?.question}
-		{$currentQuestion?.answered}
-	</h1>
+		<h1 class="text-4xl font-bold px-5 text-center">Happy 🍻-day Johan</h1>
+	</div>
+{:else}
+	<!-- else content here -->
+	<div
+		class="w-full h-full flex flex-col justify-center items-center gap-y-10 transition-colors duration-1000 ease-in-out"
+		class:bg-red-500={state == State.Wrong}
+	>
+		{#if state == State.Corerct}
+			<ConfettiExplosion />
+		{/if}
+		<h1 class="text-3xl px-5 text-center">
+			{$currentQuestion?.question}
+			{$currentQuestion?.answered}
+		</h1>
 
-	<input
-		class="bg-slate-100 p-5 border border-gray-200"
-		type="text"
-		bind:value={answer}
-		name="answer"
-		id="answer"
-		on:focus={() => {
-			state = State.Idle;
-		}}
-	/>
-	<button
-		class="bg-green-400 text-black py-3 px-8 text-lg rounded-lg font-semibold shadow-2xl"
-		on:click={valueChanged}>Answer</button
-	>
-	<button
-		class="bg-green-400 text-black py-3 px-8 text-xs rounded-lg font-semibold shadow-2xl"
-		on:click={() => {
-			goto('/add');
-		}}>Add</button
-	>
-</div>
+		<input
+			class="bg-slate-100 p-5 border border-gray-200"
+			type="text"
+			bind:value={answer}
+			name="answer"
+			id="answer"
+			on:focus={() => {
+				state = State.Idle;
+			}}
+		/>
+		<button
+			class="bg-green-400 text-black py-3 px-8 text-lg rounded-lg font-semibold shadow-2xl"
+			on:click={valueChanged}>Answer</button
+		>
+		<button
+			class="bg-green-400 text-black py-3 px-8 text-xs rounded-lg font-semibold shadow-2xl"
+			on:click={() => {
+				goto('/add');
+			}}>Add</button
+		>
+	</div>
+{/if}
